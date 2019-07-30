b22504daad78d6a71dc66cd71993f40785b83ed6
ItamarCompass <53469413+ItamarCompass@users.noreply.github.com>
2019-07-30 12:31:48 +0300

    Add files via upload

--- HomepageNumbers.js
+++ HomepageNumbers.js
@@ -1,5 +1,15 @@
 import { CountUp } from 'https://cdn.jsdelivr.net/gh/inorganik/CountUp.js@master/dist/countUp.js';
 
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
 var options;
 
 options = {
@@ -17,13 +27,13 @@ var production = new CountUp('prod-num', 2, options);
 var growth = new CountUp('growth-num', 50, options);
 var patents = new CountUp('patents-num', 300, options);
 
-$('.numbers-div').waypoint(function() {
-  countries.start();
-  cagr.start();
-  market.start();
-  production.start();
-  growth.start();
-  patents.start();
-}, {
-  offset: '100%'
-});
+var numbersSection = document.getElementById('numbers-div');
+window.addEventListener('scroll', function (event) {
+  if (isInViewport(numbersSection)) {
+    countries.start();
+    cagr.start();
+    market.start();
+    production.start();
+    growth.start();
+    patents.start();
+}}, false);
