0215e6bb4f95bfa238c71d00c734b61ac175700c
ItamarCompass <53469413+ItamarCompass@users.noreply.github.com>
2019-08-01 10:42:12 +0300

    Add files via upload

--- HomepageNumbers.js
+++ HomepageNumbers.js
@@ -5,25 +5,32 @@ var options;
 options = {
     startVal: 0,
     decimalPlaces: 0,
-    duration: 2,
+    duration: 5,
     useEasing: true,
     separseparator: ','
   };
 
 var countries = new CountUp('countries-num', 40, options);
 var cagr = new CountUp('cagr-num', 39, options);
-var market = new CountUp('market-num', 3860, options);
+var market = new CountUp('market-num', 60, options);
 var production = new CountUp('prod-num', 2, options);
 var growth = new CountUp('growth-num', 50, options);
 var patents = new CountUp('patents-num', 300, options);
 
-$('.numbers-div').waypoint(function() {
+var numbersSection = document.getElementById('numbers-div');
+
+var observer = new IntersectionObserver(function(entries) {
+	// isIntersecting is true when element and viewport are overlapping
+	// isIntersecting is false when element and viewport don't overlap
+  if(entries[0].isIntersecting === true)
+  {
   countries.start();
   cagr.start();
   market.start();
   production.start();
   growth.start();
   patents.start();
-}, {
-  offset: '100%'
-});
+  }
+}, { threshold: [0] });
+
+observer.observe(numbersSection);
